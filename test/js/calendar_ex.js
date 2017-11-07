ad2bfe3551a70fa12aa6585cd452a34377f6fef3
emra527 <eambing@ucsd.edu>
2017-11-07 11:54:18 -0800

    More Google API testing

--- test/js/calendar_ex.js
+++ test/js/calendar_ex.js
@@ -59,3 +59,59 @@ function handleAuthClick(event) {
                         handleAuthResult);
     return false;
 }
+
+/* End of PART 1 - Authentication Process. */
+
+/* Start of PART 2 - dealing with events from the user interface and
+performing API calls. */
+
+
+var addButton = document.getElementById('addToCalendar');
+addButton.onclick = function(){
+  var userChoices = getUserInput();
+  console.log(userChoices);
+  if (userChoices)
+    createEvent(userChoices);
+}
+
+function getUserInput(){
+
+  var date = document.querySelector("#date").value;
+  var startTime = document.querySelector("#start").value;
+  var endTime = document.querySelector("#end").value;
+  var eventDesc = document.querySelector("#event").value;
+
+  // check input values, they should not be empty
+  if (date=="" || startTime=="" || endTime=="" || eventDesc==""){
+    alert("All your input fields should have a meaningful value.");
+    return
+  }
+  else return {'date': date, 'startTime': startTime, 'endTime': endTime,
+               'eventTitle': eventDesc}
+}
+
+
+// Make an API call to create an event.  Give feedback to user.
+function createEvent(eventData) {
+  // First create resource that will be send to server.
+    var resource = {
+        "summary": eventData.eventTitle,
+        "start": {
+          "dateTime": new Date(eventData.date + " " + eventData.startTime).toISOString()
+        },
+        "end": {
+          "dateTime": new Date(eventData.date + " " + eventData.endTime).toISOString()
+          }
+        };
+    // create the request
+    var request = gapi.client.calendar.events.insert({
+      'calendarId': 'primary',
+      'resource': resource
+    });
+
+    // execute the request and do something with response
+    request.execute(function(resp) {
+      console.log(resp);
+      alert("Your event was added to the calendar.");
+    });
+}
